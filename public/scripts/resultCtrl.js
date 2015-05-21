daa5659fba225c7a2f64ae5f11a0819051afe06d
Qiaonan <geonann@gmail.com>
2015-05-21 14:07:27 -0400

    add enrichr function

--- public/scripts/resultCtrl.js
+++ public/scripts/resultCtrl.js
@@ -34,31 +34,78 @@ indexControllers.controller('resultCtrl',['$scope', '$routeParams', 'resultStora
 	function initialization(){
 		$scope.overlap = {};
 		$scope.tag = util.getTag($scope.input.meta);
-		
+		$scope.direction = $scope.input.config.aggravate?"mimic":"reverse";
 		if($scope.input.config.searchMethod=="geneSet"){
 			$scope.overlap.templateUrl = "partials/overlap_geneSet.html";
 			var overlapKeys = Object.keys($scope.entries[0].overlap).sort();
 			$scope.overlap.key1 = overlapKeys[1];
 			$scope.overlap.key2 = overlapKeys[0];
 			$scope.overlap.title = "overlap: input/signature";
+			$scope._keyMap = function(key){
+				return {input:key.slice(0,2),sig:key.slice(3,5)}
+			};
 			$scope.buildOverlap = function(key,entry){
 				var overlapStr = entry.overlap[key].join('\n');
-				var inputDirection = key.slice(0,2);
-				var sigDirection = key.slice(3,5);
+				var keyMap = $scope._keyMap(key)
+				var inputDirection = keyMap.input;
+				var sigDirection = keyMap.sig;
 				return overlapStr?overlapStr:("The input "+inputDirection+ " genes has no overlap with the "+sigDirection+" genes in signature.");
+			}
+			$scope.enrichr = function(key,entry){
+				var enrichrInput = {popup:true};
+				enrichrInput.list = entry.overlap[key].join('\n');
+				var values = {};
+				values.inputDirection = key.slice(0,2);
+				values.sigDirection = key.slice(3,5);
+				values.pertName = entry["pert_desc"]=="-666" || entry["pert_desc"].length > 46 ?entry["pert_id"]:entry["pert_desc"];
+				values.tag = $scope.tag?$scope.tag.value:"input";
+				var template = "overlap between {{inputDirection}} genes of {{tag}} and {{sigDirection}} genes of {{pertName}}"
+				enrichrInput.description = S(template).template(values).s;
+				util.enrich(enrichrInput);
 			}
 		}else{
 			$scope.overlap.templateUrl = "partials/overlap_CD.html";
 			var overlapKeys = Object.keys($scope.entries[0].overlap).sort();
 			$scope.overlap.key1 = overlapKeys[1];
 			$scope.overlap.key2 = overlapKeys[0];
+			$scope._aggravate = $scope.input.config.aggravate;
+			$scope._sigKeyMap = {}; // map input key to signature key
+			$scope._sigKeyMap[$scope.overlap.key1] = $scope._aggravate?$scope.overlap.key1:$scope.overlap.key2;
+			$scope._sigKeyMap[$scope.overlap.key2] = $scope._aggravate?$scope.overlap.key2:$scope.overlap.key1;
 			$scope.overlap.title = "overlap: (gene symbol) (input value) (signature value)"
-			$scope.buildOverlap = function(key,cdVec){
-				var overlapStr = _.map(_.zip($scope.uniqInput[key].genes,
-				$scope.uniqInput[key].vals, cdVec),function(arr){
-					return arr.join('\t');
-				}).join('\n');
+			$scope.buildOverlap = function(key,entry){
+				var aggravate = $scope.input.config.aggravate;
+				var cdVec = entry.overlap[key]
+				if(!("_dynamic" in entry)){
+					// store gene list
+					entry._dynamic = {};
+				}
+				var overlapArr = _.zip($scope.uniqInput[key].genes,
+				$scope.uniqInput[key].vals, cdVec);
+				var filter = aggravate?function(item){return item[1]*item[2]>0}:
+				function(item){return item[1]*item[2]<0};
+				var overlapStrArr = [];
+				entry._dynamic[key] = [];
+				overlapArr.forEach(function(item){
+					overlapStrArr.push(item.join('\t'));
+					if(filter(item)){
+						entry._dynamic[key].push(item[0]);
+					}
+				});
+				var overlapStr = overlapStrArr.join('\n');
 				return overlapStr?overlapStr:("The input "+key.slice(0,2)+ " genes has no overlap with the genes in signature.");
+			}
+			$scope.enrichr = function(key,entry){
+				var enrichrInput = {popup:true};
+				enrichrInput.list = entry._dynamic[key].join('\n');
+				var values = {};
+				values.inputDirection = key;
+				values.sigDirection = $scope._sigKeyMap[key];
+				values.pertName = entry["pert_desc"]=="-666" || entry["pert_desc"].length > 46 ?entry["pert_id"]:entry["pert_desc"];
+				values.tag = $scope.tag?$scope.tag.value:"input";
+				var template = "overlap between {{inputDirection}} genes of {{tag}} and {{sigDirection}} genes of {{pertName}}";
+				enrichrInput.description = S(template).template(values).s;
+				util.enrich(enrichrInput);
 			}
 		}
 	}
--- public/scripts/services.js
+++ public/scripts/services.js
@@ -151,5 +151,41 @@ services.factory('util',function(){
 		var timestamp = id.substring(0,8)
 		return new Date( parseInt( timestamp, 16 ) * 1000 )
 	}
+	util.enrich = function(options) {
+    	var defaultOptions = {
+    		description: "",
+    		popup: false
+  		};
+
+  		if (typeof options.description == 'undefined')
+    		options.description = defaultOptions.description;
+  		if (typeof options.popup == 'undefined')
+    		options.popup = defaultOptions.popup;
+  		if (typeof options.list == 'undefined')
+    		alert('No genes defined.');
+
+  		var form = document.createElement('form');
+  		form.setAttribute('method', 'post');
+  		form.setAttribute('action', 'http://amp.pharm.mssm.edu/Enrichr/enrich');
+  		if (options.popup)
+    		form.setAttribute('target', '_blank');
+  		form.setAttribute('enctype', 'multipart/form-data');
+
+  		var listField = document.createElement('input');
+  		listField.setAttribute('type', 'hidden');
+  		listField.setAttribute('name', 'list');
+  		listField.setAttribute('value', options.list);
+  		form.appendChild(listField);
+
+  		var descField = document.createElement('input');
+  		descField.setAttribute('type', 'hidden');
+  		descField.setAttribute('name', 'description');
+  		descField.setAttribute('value', options.description);
+  		form.appendChild(descField);
+
+  		document.body.appendChild(form);
+  		form.submit();
+  		document.body.removeChild(form);
+	}                        
 	return util;
 });
