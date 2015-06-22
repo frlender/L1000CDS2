17763ecde362476acfe3c5eb6f11bbc6ea73f314
Qiaonan <geonann@gmail.com>
2015-06-22 15:11:24 -0400

    remove drugStructure's dependency on local. add validate enrichRes

--- public/scripts/ctrls/drugStructureCtrl.js
+++ public/scripts/ctrls/drugStructureCtrl.js
@@ -1,38 +1,46 @@
-indexControllers.controller('drugStructureCtrl',['$scope', '$routeParams', 'resultStorage', 
-	'$http', 'util', 'localStorageService', '$modal', '$timeout', '$location',
-	function($scope, $routeParam, resultStorage, $http, util, local,$modal,
-		$timeout,$location){
+indexControllers.controller('drugStructureCtrl',['$scope', '$routeParams', 
+	'$http', 'util', 'Local', '$location', 'getSearch','resultStorage','validate',
+	function($scope, $routeParam, $http, util, Local,
+		$location,getSearch,resultStorage,validate){
 
-	var item = local.get($routeParam.shareID);
-	$scope.entries = item.result.topMeta;
-	$scope.input = item.input;
-	var tag = util.getTag($scope.input.meta);
-	$scope.tag = tag?tag:{value:"No tag"};
-	$scope.direction = $scope.input.config.aggravate?"mimic":"reverse";
-	$scope.entries.forEach(function(entry,i){
-			entry.rank = i+1;
-	});
-	$scope.normalizePertName = util.normalizePertName;
-	$scope.imgURL = baseURL+"CSS/images/SMARTSviewer/"
-	var pert_ids = $scope.entries.map(function(item){
-		return item.pert_id;
-	});
-	if("enrichRes" in item){
-		$scope.enrichRes = item.enrichRes;
-	}else{
-		$http.post(baseURL+'drugEnrich',pert_ids).success(function(enrichRes){
-			$scope.enrichRes = enrichRes.map(function(entry){
-				entry.overlap = entry.overlap.map(function(item){
-					return $scope.entries[pert_ids.indexOf(item)];
-				})
-				entry.overlap = _.sortBy(entry.overlap,'rank');
-				return entry
-			});
-			item.enrichRes = $scope.enrichRes;
-			local.set($routeParam.shareID,item)
+	getSearch($routeParam.shareID,function(search){
+		$scope.entries = search.result.topMeta;
+		$scope.input = search.input;
+		var tag = util.getTag($scope.input.meta);
+		$scope.tag = tag?tag:{value:"No tag"};
+		$scope.direction = $scope.input.config.aggravate?"mimic":"reverse";
+		$scope.entries.forEach(function(entry,i){
+				entry.rank = i+1;
 		});
-	}
-	
+		$scope.normalizePertName = util.normalizePertName;
+		$scope.imgURL = baseURL+"CSS/images/SMARTSviewer/"
+		var pert_ids = $scope.entries.map(function(item){
+			return item.pert_id;
+		});
+		if("enrichRes" in search){
+			if(validate.enrichRes(search.enrichRes))
+				$scope.enrichRes = search.enrichRes;
+			else $scope.err = "No substructures enriched";
+		}else{
+			$http.post(baseURL+'drugEnrich',pert_ids).success(function(enrichRes){
+				if(validate.enrichRes(enrichRes)){
+					$scope.enrichRes = enrichRes.map(function(entry){
+						entry.overlap = entry.overlap.map(function(item){
+							return $scope.entries[pert_ids.indexOf(item)];
+						})
+						entry.overlap = _.sortBy(entry.overlap,'rank');
+						return entry
+					});
+					search.enrichRes = search.enrichRes;
+					resultStorage[$routeParam.shareID] = search;
+					try{Local.prototype.set($routeParam.shareID,search);}catch(e){};
+				}else{
+					$scope.err = "No substructures enriched";
+				}
+			});
+		}
+	});
+
 	$scope.share = function(){
 		var modalInstance = $modal.open({
       		templateUrl: baseURL+'share.html',
---
+++ public/scripts/services/validate.js
@@ -0,0 +1,17 @@
+services.factory('validate',[
+	function(){
+	// validate various results.
+	return {
+		enrichRes:function(enrichRes){
+			if(enrichRes.constructor==Array &&
+				enrichRes.length > 0 &&
+				'term' in enrichRes[0] &&
+				'overlap' in enrichRes[0] &&
+				enrichRes[0].overlap.length > 0)
+				return true;
+			else return false;
+		},
+
+	}
+					
+}]);
